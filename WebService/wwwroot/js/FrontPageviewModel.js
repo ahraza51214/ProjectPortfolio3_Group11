2b74f4afc2679e8f818019a0cc1ecc51e7d938a3
konstantinovcharov <konstantinovcharov@abv.bg>
2020-12-08 09:28:41 +0100

    bobX branch from monday noon + not working front page component

---
+++ WebService/wwwroot/js/FrontPageviewModel.js
@@ -0,0 +1,20 @@
+﻿define(['knockout'], (ko) => {
+    
+    let selectedComponent = ko.observable('login');
+
+    let changeContent = () => {
+        if (selectedComponent() === "login") {
+            selectedComponent('signup');
+        } else {
+            
+            selectedComponent('login');
+        }
+    }
+
+    
+
+    return {
+        selectedComponent,
+        changeContent
+    };
+});
--- WebService/wwwroot/js/main.js
+++ WebService/wwwroot/js/main.js
@@ -3,17 +3,37 @@
 require.config({
     baseUrl: "js",
     paths : {
-        knockout: "lib/knockout/knockout-latest"
+        knockout: "lib/knockout/knockout-latest",
+        text: "lib/require-text/text.min"
     }
+});
+
+require(['knockout', 'text'], (ko) => {
+    ko.components.register("fp", {
+        viewModel: { require: "components/fp/fp"},
+        template: { require: "text!components/fp/fp.html" }
+    });    
+});
+
+require(['knockout', 'text'], (ko) => {
+    ko.components.register("login", {
+        viewModel: { require: "components/login/login" },
+        template: { require: "text!components/login/login.html" }
+    });
+});
+
+require(['knockout', 'text'], (ko) => {
+    ko.components.register("signup", {
+        viewModel: { require: "components/signup/signup" },
+        template: { require: "text!components/signup/signup.html" }
+    });
 });
 
 
 
-
-
-require(['knockout', 'BookmarkviewModel', 'UserviewModel', 'SearchviewModel'], function (ko, bookmarkviewModel, userviewModel, searchviewModel) {
+require(['knockout', 'BookmarkviewModel', 'UserviewModel', 'SearchviewModel', 'FrontPageviewModel'], function (ko, bookmarkviewModel, userviewModel, searchviewModel, FrontPageviewModel) {
     var viewmodel = {
-        bookmarkviewModel, userviewModel,searchviewModel
+        bookmarkviewModel, userviewModel, searchviewModel, FrontPageviewModel
     }
    
     ko.applyBindings(viewmodel);
