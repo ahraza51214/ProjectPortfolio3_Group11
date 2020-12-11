758c170adfca70a1ddee6e4dd002c8371643a3c1
konstantinovcharov <konstantinovcharov@abv.bg>
2020-12-11 15:57:56 +0100

    changed viewmodels to controllers

--- WebService/wwwroot/js/components/rating/rating.js
+++ WebService/wwwroot/js/components/rating/rating.js
@@ -1,7 +1,8 @@
 ﻿define(['knockout'], function (ko) {
-    
+    return function () {
+
     let ratings = ko.observableArray(
-        [{userId: "", titleIndividRating: "", tConst: "", userTitleRateDate: ""}]);
+        [{ userId: "", titleIndividRating: "", tConst: "", userTitleRateDate: "" }]);
 
     let tconst = ko.observable();
     let userId = ko.observable();
@@ -13,7 +14,7 @@
         let headers = new Headers();
         headers.append("Content-Type", "application/json");
         fetch("api/rating", {
-            method: "POST", body: JSON.stringify({ tConst: tconst(), userId: +userId(), titleIndividRating: +titleIndividRating()}), headers
+            method: "POST", body: JSON.stringify({ tConst: tconst(), userId: +userId(), titleIndividRating: +titleIndividRating() }), headers
         })
             .then(response => response.json())
     }
@@ -21,32 +22,33 @@
 
     let getRating = function () {
         fetch("api/rating/" + userId())
-            
+
             .then(function (response) {
 
-                    if (response.status === 404 || response.status === 400) {
-                        responseMessage("Invalid request! Please type a valid userId!");
-                        ratings("");
-                        throw new Error(response.status + " Rating Not found ");
-                    }
-                    if (response.status === 200) {
-
-                        responseMessage("Rating retrieved succesfully!");
-                    }
-                    
-                    return response.json();
+                if (response.status === 404 || response.status === 400) {
+                    responseMessage("Invalid request! Please type a valid userId!");
+                    ratings("");
+                    throw new Error(response.status + " Rating Not found ");
                 }
+                if (response.status === 200) {
+
+                    responseMessage("Rating retrieved succesfully!");
+                }
+
+                return response.json();
+            }
 
             )
             .then(function (data) {
                 ratings(data);
             });
     }
-    
+
     return {
         tconst, userId, titleIndividRating,
 
         ratings, getRating, responseMessage,
         createRating
-    };
+        };
+    }
 });
--- WebService/wwwroot/js/main.js
+++ WebService/wwwroot/js/main.js
@@ -1,22 +1,56 @@
-﻿
-
-require.config({
+﻿require.config({
     baseUrl: "js",
-    paths : {
-        knockout: "lib/knockout/knockout-latest"
+    paths: {
+        knockout: "lib/knockout/knockout-latest",
+        text: "lib/require-text/text.min"
     }
 });
 
+require(['knockout', 'text'], (ko) => {
+    ko.components.register("fp", {
+        viewModel: { require: "components/fp/fp" },
+        template: { require: "text!components/fp/fp.html" }
+    });
+});
 
-require(['knockout', 'BookmarkviewModel', 'UserviewModel', 'SearchviewModel', 'RatingViewModel'], function (ko, bookmarkviewModel, userviewModel, searchviewModel, ratingViewModel) {
-    var viewmodel = {
-        bookmarkviewModel, userviewModel,searchviewModel, ratingViewModel
-    }
-   
-    ko.applyBindings(viewmodel);
+require(['knockout', 'text'], (ko) => {
+    ko.components.register("login", {
+        viewModel: { require: "components/login/login" },
+        template: { require: "text!components/login/login.html" }
+    });
+});
 
+require(['knockout', 'text'], (ko) => {
+    ko.components.register("signup", {
+        viewModel: { require: "components/signup/signup" },
+        template: { require: "text!components/signup/signup.html" }
+    });
+});
+
+require(['knockout', 'text'], (ko) => {
+    ko.components.register("search", {
+        viewModel: { require: "components/search/search" },
+        template: { require: "text!components/search/search.html" }
+    });
+});
+
+require(['knockout', 'text'], (ko) => {
+    ko.components.register("bookmark", {
+        viewModel: { require: "components/bookmark/bookmark" },
+        template: { require: "text!components/bookmark/bookmark.html" }
+    });
+});
+
+require(['knockout', 'text'], (ko) => {
+    ko.components.register("rating", {
+        viewModel: { require: "components/rating/rating" },
+        template: { require: "text!components/rating/rating.html" }
+    });
 });
 
 
+require(['knockout', 'FrontPageviewModel'], function (ko, fpvm) {
 
+    ko.applyBindings(fpvm);
 
+});
